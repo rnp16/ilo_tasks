53b47b6535cb27daec606e9c173319564131302b
SeanTecha <sean.t@puppet.com>
2017-10-18 09:24:12 -0700

    Cannot use get-executionpolicy, going straight to the value on the machine

--- tasks/get_executionpolicy.ps1
+++ tasks/get_executionpolicy.ps1
@@ -1,1 +1,5 @@
-Get-ExecutionPolicy
+# Not using Get-ExecutionPolicy because that shows 'Bypass' due to the remote connection
+# instead of the actual execution policy on local system.
+
+$key = 'HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell'
+Write-Output (Get-ItemProperty -Path $key).ExecutionPolicy
