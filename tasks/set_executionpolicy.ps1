a2be88ff2c31885e6abca3882e76ff84e404b4da
SeanTecha <sean.t@puppet.com>
2017-10-18 15:34:59 -0700

    testing set-executionpolicy

--- tasks/set_executionpolicy.ps1
+++ tasks/set_executionpolicy.ps1
@@ -1,10 +1,12 @@
 [CmdletBinding()]
 Param(
-  [Parameter(Mandatory = $True)]
+  [Parameter(Mandatory = $False)]
  [String]
   $policy
-)
+  )
 
 if ($policy -eq "bypass") {
   Set-ExecutionPolicy Bypass -Force
+  $key = 'HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell'
+  Write-Output (Get-ItemProperty -Path $key).ExecutionPolicy
 }
