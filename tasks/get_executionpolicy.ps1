faf93eb75ea1c12cb4c80e8735c705bc07d4eaf9
SeanTecha <sean.t@puppet.com>
2017-11-07 10:13:48 -0800

    updating metadata

--- tasks/get_executionpolicy.ps1
+++ tasks/get_executionpolicy.ps1
@@ -1,5 +1,8 @@
 # Not using Get-ExecutionPolicy because that shows 'Bypass' due to the remote connection
 # instead of the actual execution policy on local system.
 
-$key = 'HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell'
-Write-Output (Get-ItemProperty -Path $key).ExecutionPolicy
+$HKLMkey = 'HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell'
+$HKCUkey = 'HKCU:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell'
+
+Write-Output "LocalMachine $((Get-ItemProperty -Path $HKLMkey).ExecutionPolicy)"
+Write-Output "CurrentUser $((Get-ItemProperty -Path $HKCUkey).ExecutionPolicy)"
