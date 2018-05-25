3279e27bb4228c6e5099bec0c9e5983baefbef4e
rnp16 <rnp16@zips.uakron.edu>
2018-05-25 10:28:44 -0400

    Delete get_executionpolicy.ps1

--- tasks/get_executionpolicy.ps1
+++
@@ -1,8 +0,0 @@
-# Not using Get-ExecutionPolicy because that shows 'Bypass' due to the remote connection
-# instead of the actual execution policy on local system.
-
-$HKLMkey = 'HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell'
-$HKCUkey = 'HKCU:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell'
-
-Write-Output "LocalMachine $((Get-ItemProperty -Path $HKLMkey).ExecutionPolicy)"
-Write-Output "CurrentUser $((Get-ItemProperty -Path $HKCUkey).ExecutionPolicy)"
